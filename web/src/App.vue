b3bb1cfaf4ee1ace2ead36c05f23ab0eb0f21ee1
WangJinqi <wangjinqi@agora.io>
2021-03-13 18:30:21 +0800

    feature():引入vue3前端，进行组件化提取

---
+++ web/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-layout>
+    <the-header></the-header>
+    <router-view/>
+    <the-footer></the-footer>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
+
+export default defineComponent({
+  name: 'app',
+  components:{
+    TheHeader,
+    TheFooter
+  }
+});
+</script>
+
+<style>
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
+</style>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki ©2021 Created by iiisunny
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
