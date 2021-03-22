46b6ac8ff00b9730ab208f982a73912a3ad0cd23
WangJinqi <4487071@qq.com>
2021-03-22 22:51:31 +0800

    revert():回滚提交

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/home.vue'
-import About from '../views/about.vue'
-import AdminEbook from '../views/admin/admin-ebook.vue'
+import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,16 +10,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/about.j')
-  },
-  {
-    path: '/admin/ebook',
-    name: 'AdminEbook',
-    component: AdminEbook
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
