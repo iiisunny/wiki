13f8c99b8bb38f8f1f6e909d10464d834761565c
iiisunny <4487071@qq.com>
2021-06-20 23:06:12 +0800

    style():更新标题

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-footer style="text-align: center">
-    知识库<span v-show="user.id">，欢迎：{{user.name}}</span>
+    测试平台<span v-show="user.id">，欢迎：{{user.name}}</span>
   </a-layout-footer>
 </template>
 
